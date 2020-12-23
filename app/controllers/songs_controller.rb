d3fa16486a316bd733452a7488080c7ddf01ac12
Sophie Gu <sophieqgu@gmail.com>
2020-12-23 04:18:01 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      @artist = Artist.find(id: params[:artist_id])
+      @artist = Artist.find(params[:artist_id])
       if @artist.nil?
         redirect_to artists_path, alert: "Artist not found."
       else
@@ -14,7 +14,7 @@ class SongsController < ApplicationController
 
   def show
     if params[:artist_id]
-      @artist = Artist.find_by(id: params[:artist_id])
+      @artist = Artist.find_by(params[:artist_id])
       @song = @artist.songs.find_by(params[:id])
       if @song.nil?
         redirect_to artist_songs_path(@artist), alert: "Song not found."
